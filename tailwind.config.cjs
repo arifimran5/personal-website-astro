5439a62a8b86c61f627a4a6bc34b4a27b5728d27
arifimran5 <arifimran505@gmail.com>
2023-07-01 20:35:24 +0530

    hero section and project section done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ["system-ui"],
+      sans: ["Inter", "system-ui"],
       mono: ["Menlo", "Liberation Mono", "monospace"]
     },
     extend: {
+      colors: {
+        text_primary: "#EEEEEE",
+        text_secondary: "#A4A4A4",
+        accent: "#E7FFA3"
+      }
     },
   },
   plugins: [],
