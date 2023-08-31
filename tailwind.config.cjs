40f964f83f440f89b3583ac8a1965d14b1d21680
arifimran5 <arifimran505@gmail.com>
2023-09-01 01:58:47 +0530

    add handjet font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,18 +3,17 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ["Inter", "system-ui"],
-      mono: ["Menlo", "Liberation Mono", "monospace"]
+      sans: ['Inter', 'system-ui'],
+      fun: ['handjet'],
+      mono: ['Menlo', 'Liberation Mono', 'monospace'],
     },
     extend: {
       colors: {
-        text_primary: "#EEEEEE",
-        text_secondary: "#A4A4A4",
-        accent: "#E7FFA3"
-      }
+        text_primary: '#EEEEEE',
+        text_secondary: '#A4A4A4',
+        accent: '#E7FFA3',
+      },
     },
   },
   plugins: [],
 }
-
-
