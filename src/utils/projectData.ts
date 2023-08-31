a3ca3becbafd9453fef21606bb4cdb6a404faa1e
arifimran5 <arifimran505@gmail.com>
2023-09-01 01:58:52 +0530

    add cinegeek v2

--- src/utils/projectData.ts
+++ src/utils/projectData.ts
@@ -1,6 +1,15 @@
 export const projects = [
   {
     id: 1,
+    title: "cinegeek v2",
+    description:
+      "Rewrite of cinegeek with NextJS 13 React Server Components",
+    technologies: ["React", "NextJS", "TailwindCSS", "TMDB API"],
+    github: "https://github.com/arifimran5/cinegeekV2",
+    live: "https://cinegeek.vercel.app",
+  },
+  {
+    id: 2,
     title: "twipper",
     description: "A microblogging web app just like `you know what`.",
     technologies: [
@@ -15,7 +24,7 @@ export const projects = [
     live: "https://twipper.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     title: "cinegeek",
     description:
       "A movie listing application that provides details about movies based on popularity, rating, and different genres.",
