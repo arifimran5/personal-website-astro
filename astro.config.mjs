6955457ba9b8e67388387357e443341f1064b51f
arifimran5 <arifimran505@gmail.com>
2023-09-12 21:37:13 +0530

    configured mdx and add images to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'github-dark',
-      langs: [],
-    },
-  },
-})
+      langs: []
+    }
+  }
+});
