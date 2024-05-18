5a6b9dfe3cb867ae50062cf84e9c0ba9d844ec46
arifimran5 <arifimran505@gmail.com>
2024-05-18 15:48:08 +0530

    rm server rendering,gtag,partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,11 @@ import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-// import vercel from '@astrojs/vercel/serverless'
-// import partytown from '@astrojs/partytown'
 import robotsTxt from 'astro-robots-txt'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://thatarif.in',
-  integrations: [
-    tailwind(),
-    react(),
-    mdx(),
-    sitemap(),
-    // partytown({
-    //   config: {
-    //     forward: ['dataLayer.push'],
-    //   },
-    // }),
-    robotsTxt(),
-  ],
+  integrations: [tailwind(), react(), mdx(), sitemap(), robotsTxt()],
   output: 'static',
   markdown: {
     syntaxHighlight: 'shiki',
